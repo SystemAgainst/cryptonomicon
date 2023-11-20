ea37e6bbcdc4c02f59dcdf33305b338e62350367
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2023-11-20 18:36:11 +0300

    fix: added check for back button

--- src/App.vue
+++ src/App.vue
@@ -172,6 +172,7 @@ export default {
           <button
             class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             @click="page -= 1"
+            v-if="page > 1"
           >
             Назад
           </button>
